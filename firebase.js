07ddc63c5f9d3219f033192d05d1392095e94712
SOFEVIP98 <Sofeil938393@gmail.com>
2026-08-02 17:58:47 -0700

    Initialize Firebase app and set up authentication

---
+++ firebase.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.17.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3bTd4yA869hoH_nhbaEigXvxDBJYZoa0",
+  authDomain: "accountstore-429f8.firebaseapp.com",
+  projectId: "accountstore-429f8",
+  storageBucket: "accountstore-429f8.firebasestorage.app",
+  messagingSenderId: "63564338179",
+  appId: "1:63564338179:web:c964eb07be2cde7e39aa36"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
+
+export {
+    signInWithPopup,
+    signOut,
+    onAuthStateChanged
+};
